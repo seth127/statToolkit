9b4897368383de15880500d142d135695cc3d2db
Seth Green <seth127@gmail.com>
2016-10-18 07:23:57 -0400

    fixed ridge and added lasso

--- RidgeAndLasso.R
+++ RidgeAndLasso.R
@@ -76,9 +76,81 @@ ridgeit <- function(df) { ### RESPONSE MUST BE NAMED 'y'
   glmnet(x, y, alpha=0, lambda=bestlam)
 }
 
-#gasmod <- ridgeit(gas)
+#ridgemod <- ridgeit(gas)
 
-ridgepred <- function(mod, newdata) {
+
+lassoit <- function(df) { ### RESPONSE MUST BE NAMED 'y'
+  #library(car)
+  #library(glmnet)
+  #dfm <- as.matrix(df)
+  x <- model.matrix(y~., data=df)[,2:ncol(df)]
+  y <- df$y
+  
+  df.lasso <- glmnet(x, y, alpha=1)
+  
+  ## lasso coefficicent estimates
+  #coef(df.lasso)
+  
+  # pick optimal lambda with cross-validated test set MSE
+  set.seed (1)
+  cv.out = cv.glmnet(x, y, alpha=1, nfolds=nrow(x))
+  plot(cv.out)
+  bestlam=cv.out$lambda.min
+  print(paste("Best Lambda:", bestlam))
+  
+  ## Create the lasso trace plot
+  plot(df.lasso,xvar="lambda",label=TRUE)
+  abline(v=log(bestlam), lty=2)
+  
+  ## fit with best lambda
+  best.lasso <- glmnet(x, y, alpha=1, lambda=bestlam)
+  
+  ## look at residual sum of squares
+  lasso.preds <- predict(best.lasso ,s=bestlam ,newx=x)
+  orig.preds <- predict(lm(y ~ x))
+  
+  lasso.SSres <- sum( (y - lasso.preds)^2 )
+  orig.SSres <- sum( (y - orig.preds)^2 )
+  
+  print("SSres")
+  print(paste("Lasso:", lasso.SSres))
+  print(paste("Original:", orig.SSres))
+  
+  ## R^2
+  
+  lasso.R2 <- sum((lasso.preds - mean(y))^2) / sum((y - mean(y))^2)
+  orig.R2 <- sum((orig.preds - mean(y))^2) / sum((y - mean(y))^2)
+  
+  print("R^2")
+  print(paste("Lasso:", lasso.R2))
+  print(paste("Original:", orig.R2))
+  
+  # check a held out test set MSE
+  train=sample(1:nrow(df), nrow(df)*.75)
+  
+  trainx <- x[train, ]
+  trainy <- y[train]
+  
+  testx <- x[-train, ]
+  testy <- y[-train]
+  
+  tt.lasso <- glmnet(trainx, trainy, alpha=1, lambda=bestlam)
+  tt.orig <- lm(y ~ ., data=df[train,])
+  
+  # print test MSE
+  MSE.lasso <- sum((testy - predict(tt.lasso,s=bestlam,newx=testx))^2) / nrow(testx)
+  MSE.orig <- sum((testy - predict(tt.orig,newdata=df[-train,]))^2) / nrow(testx)
+  print("Test Set MSE")
+  print(paste("Lasso:", MSE.lasso))
+  print(paste("Original:", MSE.orig))
+  
+  #return the optimal model
+  glmnet(x, y, alpha=1, lambda=bestlam)
+}
+
+lassomod <- lassoit(gas)
+
+rlpred <- function(mod, newdata) {
   if('y' %in% names(newdata)) {
     newdata <- subset(newdata, select = -y) # get rid of y, if it's included
   }
@@ -86,6 +158,7 @@ ridgepred <- function(mod, newdata) {
   as.numeric(predict(mod, newx=mm))
 }
 
-## test it
-#plot(gas$x5, gas$y)
-#points(gas$x5, ridgepred(gasmod, gas), col="red")
+# ## test it
+# plot(gas$x5, gas$y)
+# points(gas$x5, rlpred(ridgemod, gas), col="red")
+# points(gas$x5, rlpred(lassomod, gas), col="blue")
